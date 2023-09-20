19c78b7c5f7fdd9323ffb05d2147fa7053ca089c
jhj04 <129846546+jhj04@users.noreply.github.com>
2023-09-21 02:13:51 +0900

    Update chapter1-2_assignment.dart

--- 정현주/1주차/과제/chapter1-2_assignment.dart
+++ 정현주/1주차/과제/chapter1-2_assignment.dart
@@ -31,3 +31,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// 1 강에서의 학습 내용을 바탕으로 특히, bottomNavigationBar 구현이 메인으로 되어있는 과제라고 생각되었습니다.
+// 초기 bottomNavigationBar을 설정했을 때, bar의 높이가 너무 낮아 부자연스럽고 어색한 모습이었기 때문에 bar의 높이를 70으로 높여 설정해주었으며,
+//  세 개의 아이콘이 세로로 일정한 간격으로 정렬될 수 있도록 MainAxisAlignment.spaceEvenly를 사용해주었습니다.
