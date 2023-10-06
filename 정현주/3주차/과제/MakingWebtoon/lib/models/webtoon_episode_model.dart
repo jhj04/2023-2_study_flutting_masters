07439f173af72951df7a4f338136f296d0f8c8ae
jhj04 <129846546+jhj04@users.noreply.github.com>
2023-10-06 22:41:09 +0900

    Add files via upload

---
+++ 정현주/3주차/과제/MakingWebtoon/lib/models/webtoon_episode_model.dart
@@ -0,0 +1,10 @@
+class WebtoonEpisodeModel {
+  final String id, title, rating, date;
+
+  WebtoonEpisodeModel.fromJson(Map<String,dynamic>json):
+
+  title = json['title'],
+  rating = json['rating'],
+  date = json['date'],
+  id = json['id'];
+}
